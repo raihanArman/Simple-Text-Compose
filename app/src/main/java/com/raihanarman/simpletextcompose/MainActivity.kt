b98adc1114ac8febfd35e1a64f2b7f9eab76de13
raihanArman <raihanarman8@gmail.com>
2021-12-08 09:22:34 +0700

    add button

--- app/src/main/java/com/raihanarman/simpletextcompose/MainActivity.kt
+++ app/src/main/java/com/raihanarman/simpletextcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.raihanarman.simpletextcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,12 +15,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleTextComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Kevin")
-                }
-            }
+            GreetingButton()
         }
     }
 }
@@ -29,10 +25,16 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun GreetingButton(){
+    Button(onClick = {  }) {
+        Greeting(name = "button")
+        Greeting(name = "kevin")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    SimpleTextComposeTheme {
-        Greeting("Kevin")
-    }
+fun DefaultPreviewMainActivity() {
+    GreetingButton()
 }
